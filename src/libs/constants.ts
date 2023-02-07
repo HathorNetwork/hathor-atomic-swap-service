8e2ccda02dccb5409ec1d54365c1092b0de482d3
tuliomir <tulio.mir@gmail.com>
2023-02-06 23:18:28 -0300

    docs: warning on the importance of hash configs

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -5,14 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+//-------------------------------------------------------
+// IMPORTANT:
+// The HASH_ITERATIONS and HASH_KEY_SIZE constants below determine how the hashed auth passwords are generated and, most importantly, validated.
+// Changing any of these values will make all existing passwords on the database invalid and deny access to them to all the proposals users.
+//-------------------------------------------------------
+
 /**
  * Number of iterations to execute when hashing the password
  *
  * NIST recommends at least 10,000 iterations (https://pages.nist.gov/800-63-3/sp800-63b.html#sec5),
+ * @see Comment above before changing this value
  */
 export const HASH_ITERATIONS = 10000;
 
 /**
  * Size of the key to hash the password
+ * @see Comment above before changing this value
  */
 export const HASH_KEY_SIZE = 256;
