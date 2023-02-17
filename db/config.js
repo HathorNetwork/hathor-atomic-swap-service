2309436dee914a1aeef55ed1684e13e81b15019d
tuliomir <tulio.mir@gmail.com>
2023-02-16 21:50:43 -0300

    refactor: simplifies sequelize config

--- db/config.js
+++ db/config.js
@@ -8,32 +8,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 
-let development;
-
-switch (process.env.DEV_DB) {
-  case 'mysql':
-    development = {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: '127.0.0.1',
-      port: process.env.DB_PORT || 3306,
-      dialect: 'mysql',
-      dialectOptions: {
-        bigNumberStrings: true,
-      },
-    };
-    break;
-  default:
-    development = {
-      storage: process.env.DB_FILE || './db.sqlite3',
-      dialect: 'sqlite',
-    };
-    break;
-}
-
 module.exports = {
-  development,
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
   test: {
     username: process.env.CI_DB_USERNAME,
     password: process.env.CI_DB_PASSWORD,
