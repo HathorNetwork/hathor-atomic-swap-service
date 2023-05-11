c3a873ea223214080fd7263ee1fb2d6eb43d6b3e
tuliomir <tulio.mir@gmail.com>
2023-05-11 11:26:26 -0300

    docs: handler comments

--- src/functions/websocket/handler.ts
+++ src/functions/websocket/handler.ts
@@ -17,15 +17,9 @@ import middy from '@middy/core';
 import { errorHandlerMiddleware, sqlConnectionMiddleware } from '@libs/lambda';
 
 const connectFunction = async (event) => {
-  // console.log(`\n${JSON.stringify(event)}\n`);
-  const routeKey = event.requestContext.routeKey;
-  // info needed to send response to client
   const connInfo = connectionInfoFromEvent(event);
-  console.log(`Received ${routeKey} with connInfo ${JSON.stringify(connInfo)}`);
-
   await initWsConnection(event.mySql, connInfo);
 
-  console.log(`Returned from "${routeKey}" successfully`);
   return DEFAULT_API_GATEWAY_RESPONSE;
 };
 export const connectHandler = middy(connectFunction)
@@ -33,15 +27,9 @@ export const connectHandler = middy(connectFunction)
   .use(errorHandlerMiddleware());
 
 const disconnectFunction = async (event): Promise<APIGatewayProxyResult> => {
-  // console.log(`\n${JSON.stringify(event)}\n`);
-  const routeKey = event.requestContext.routeKey;
-  // info needed to send response to client
   const connInfo = connectionInfoFromEvent(event);
-  console.log(`Received ${routeKey} with connInfo ${JSON.stringify(connInfo)}`);
-
   await endWsConnection(event.mySql, connInfo.id);
 
-  console.log(`Returned from "${routeKey}" successfully`);
   return DEFAULT_API_GATEWAY_RESPONSE;
 };
 export const disconnectHandler = middy(disconnectFunction)
@@ -49,15 +37,9 @@ export const disconnectHandler = middy(disconnectFunction)
   .use(errorHandlerMiddleware());
 
 const pingFunction = async (event): Promise<APIGatewayProxyResult> => {
-  // console.log(`\n${JSON.stringify(event)}\n`);
-  const routeKey = event.requestContext.routeKey;
-  // info needed to send response to client
   const connInfo = connectionInfoFromEvent(event);
-  console.log(`Received ${routeKey} with connInfo ${JSON.stringify(connInfo)}`);
-
   await sendMessageToClient(event.mySql, connInfo, { type: 'pong' });
 
-  console.log(`Returned from "${routeKey}" successfully`);
   return DEFAULT_API_GATEWAY_RESPONSE;
 };
 export const pingHandler = middy(pingFunction)
